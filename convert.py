52df3f333f93cc79cd892c213ebc670a39201c39
T.J <thomas.turnage@noaa.gov>
2019-11-05 08:44:08 -0500

    11/05/2019
    - added dictionary of crop parameters and new filename extensions to aid in looping through crops

--- convert.py
+++ convert.py
@@ -16,17 +16,23 @@ def myCrop(cropStr, src_file, dst_dir, dst_extension):
     dst_file = os.path.join(dst_dir,out_file)
     print(dst_file)
     command = "convert -crop " + cropStr + " +repage " + src_file + ' ' + dst_file
-    #os.system(command)
+    print(command)
+    os.system(command)
     return dst_file
 
 
-src_dir = 'C:/data/temp'
+src_dir = 'C:/data/NBM-ver/screen'
 dst_dir = os.path.join(src_dir,'modified')
 os.makedirs(dst_dir, exist_ok=True)
 p = re.compile('PNG')
 
+image = {'ndfd':{'crop':'730x435+10+6','extension':'_ndfd.PNG'},
+         'nbm_31':{'crop':'666x438+740+6','extension':'_nbm31.PNG'},
+         'nbm_32':{'crop':'731x413+735+430','extension':'_nbm32.PNG'},
+         'nohrsc':{'crop':'730x369+9+471','extension':'_nohrsc.PNG'}}
+
 #crop_string = '1589x937+6+104'
-crop_string = '666x438+261+602'   # nohrsc
+#crop_string = '666x438+261+602'   # nohrsc
 #crop_string = '651x471+258+102' #ndfd
 #crop_string = '631x936+926+107' #nbm
 montage_files = []
@@ -34,12 +40,17 @@ montage_files = []
 os.chdir(src_dir)
 for src_file in glob.glob('*PNG'):
     m = p.search(src_file)
-    if m is not None:
+    if 2 > 1:
+    #if m is not None:
         #print(src_file)
         #src = open(srcFile, "r")
         src_file_path = os.path.join(src_dir,str(src_file))
-        cropStr = crop_string 
-        dst_file = myCrop(crop_string, src_file, dst_dir,'_nohrsc.PNG')
+        for key in image:
+            print(key)
+            crop_string = image[key]['crop']
+            ext = image[key]['extension']
+            print(crop_string,ext)
+            dst_file = myCrop(crop_string, src_file, dst_dir, ext)
 
 
 os.chdir(dst_dir)
@@ -47,14 +58,19 @@ for card in ('f24*','f36*','f48*','f60*'):
     string = ' ' 
     for mf in glob.glob(card):
         #montage_files.append(os.path.join(dst_dir,mf))
+        geom = ' '
         print(os.path.join(dst_dir,mf))
+#        if 'nbm' in mf:
+#            geom = ' -geometry 300x300+0+0 '
+#        else:
+#            geom = ' -geometry 400x400+300-6 '            
+        
         string = string + ' ' + os.path.join(dst_dir,mf)
     montage_file = 'montage_' + card[:-1] + '.png'
 
-    cmd = 'montage' + string  + ' -geometry 300x300 ' +  os.path.join(dst_dir,montage_file)
+    cmd = 'montage' + string  + geom +  os.path.join(dst_dir,montage_file)
     os.system(cmd)
     print(cmd)
-    print('\n\n')
 
-montage_sorted = sorted(montage_files)
-print(montage_sorted)
+#montage_sorted = sorted(montage_files)
+#print(montage_sorted)
