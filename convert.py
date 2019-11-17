ac5f49a70c1ad4ea0f54ce0e65f5e3aec5a13e86
T.J <thomas.turnage@noaa.gov>
2019-11-17 14:16:28 -0500

    11/17/2019
    major changes including ability to create montages of all forecast hours (and verification) for a nygiven forecast element and data source (model)

--- convert.py
+++ convert.py
@@ -1,76 +1,214 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  4 11:47:11 2019
+This script takes png screen shots of NBM viewer data,
+crops the 4 panes, and creates mosaics for each forecast
+hour containing the different forecast sources as well as verification 
+with data in the following configuration
 
-@author: thomas.turnage
+   top  left : NDFD
+   top right : NBMv32
+ bottom left : verifiying dataset (URMA, NOHRSC, etc.)
+ bottom left : NBMv31
+
+NBM viewer settings
+                   Type : Event Review
+               Location : GRR or MI
+    Analysis Hour (UTC) : 12Z works the best
+                  Range : Mid 
+   NDFD Issuance offset : 06 to reflect the forecast
+                          being based on 6 hours earlier guidance
+
+It's easiest to take a screen shot of the entire browser window
+and then use Paint or some other application to find the x,y pixel
+coordinates for the top left corners of the crop areas associated
+with top left and bottom right panes in the screen shot
+
+The filenames of these screen-captured images need to follow this example:
+    
+    SnowAmt24_20191113-12Z_F72.PNG
+
+      SnowAmt24 : forecast element (easiest to emulate NBM naming
+                  convention; in this case, 24 hour snowfall)
+       20191113 : verifying date
+            12Z : verifying hour
+            F72 : hours in future from forecast time
+
 """
 
 import glob
 import os
-import re
 
 
-def myCrop(cropStr, src_file, dst_dir, dst_extension):
-    out_file = src_file[:-4] + dst_extension
-    #print(out_file)
-    dst_file = os.path.join(dst_dir,out_file)
-    print(dst_file)
-    command = "convert -crop " + cropStr + " +repage " + src_file + ' ' + dst_file
-    print(command)
-    os.system(command)
-    return dst_file
+def myResize(resizeImages, resizeValue, run):
+    if run is True:
+        resizeCommand = "mogrify -resize " + resizeValue + " " + resizeImages
+        os.system(resizeCommand);
+        print ("done resizing")
+    return
 
+def myCrop(src_path,dst_path,crop_string, run):
+    """
+    src_file_dir : example - 'C:/data/{case_name}
+    example: SnowAmt24_20191113-12Z_F24.PNG
+    """  
+    command = "convert -crop " + crop_string + " +repage " + src_path + ' ' + dst_path
+    if run is True:
+        print(command)
+        os.system(command)
+    
+    return
 
-src_dir = 'C:/data/NBM-ver/screen'
+def uniq(my_list):
+    """
+    Takes a list, sorts it, elimates duplicates, and returns new list
+    """
+    my_list.sort()
+    output = []
+    for x in my_list:
+        if x not in output:
+            output.append(x)
+    output.sort()
+    return output
+
+src_dir = 'C:/data/20191111/verif/test'
 dst_dir = os.path.join(src_dir,'modified')
 os.makedirs(dst_dir, exist_ok=True)
-p = re.compile('PNG')
 
-image = {'ndfd':{'crop':'730x435+10+6','extension':'_ndfd.PNG'},
-         'nbm_31':{'crop':'666x438+740+6','extension':'_nbm31.PNG'},
-         'nbm_32':{'crop':'731x413+735+430','extension':'_nbm32.PNG'},
-         'nohrsc':{'crop':'730x369+9+471','extension':'_nohrsc.PNG'}}
+"""
+Below is for GRR, then zoom out one step.
+Also deselect annoying CWA boundary map
 
-#crop_string = '1589x937+6+104'
-#crop_string = '666x438+261+602'   # nohrsc
-#crop_string = '651x471+258+102' #ndfd
-#crop_string = '631x936+926+107' #nbm
+upper_left = [511,256]
+lower_right = [1239,715]
+img_width = 340
+img_height = 325
+"""
+
+"""
+Below is for MI projection, request it be shifted a little farther
+south instead of right at IN border.
+"""
+#top_left = [497,228]        # top left corner of crop area for top left image 
+
+bottom_right = [1227,686]   # top left corner of crop area for bottom right image
+img_width = 325
+img_height = 388
+
+top_left = [496,207]
+bottom_right = [1226,666]
+
+
+
+top_left = ['+'+str(a) for a in top_left]
+bottom_right = ['+'+str(b) for b in bottom_right]
+img_width = 300
+img_height = 400
+
+crop_dim = str(img_width) + 'x' + str(img_height)
+x = [top_left[0],bottom_right[0]]
+y = [top_left[1],bottom_right[1]]
+
+coor_list = [crop_dim+a+b for b in y for a in x]
+
+image = {'ndfd':{'crop':coor_list[0],'extension':'ndfd'},               # upper left
+         'nbm32':{'crop':coor_list[1],'extension':'nbm32'},             # upper right
+         #'nohrsc':{'crop':coor_list[2],'nohrsc.PNG'},                  
+         'observed':{'crop':coor_list[2],'extension':'observed'},       # lower left
+         'nbm31':{'crop':coor_list[3],'extension':'nbm31'}              # lower right
+}
+
 montage_files = []
-
+fcst_hour_list = []
+valid_time_list = []
+forecast_element_list = []
 os.chdir(src_dir)
 for src_file in glob.glob('*PNG'):
-    m = p.search(src_file)
-    if 2 > 1:
-    #if m is not None:
-        #print(src_file)
-        #src = open(srcFile, "r")
-        src_file_path = os.path.join(src_dir,str(src_file))
+    """
+    example: SnowAmt24_20191113-12Z_F24.PNG
+    """  
+    if 'montage' not in src_file:
+        src_path = os.path.join(src_dir,str(src_file))
+        dst_dir = os.path.join(src_dir,'modified')
+
+  
+        fname_split = str(src_file).split('_')
+
+        forecast_element = str(fname_split[0])          # SnowAmt24
+        forecast_element_list.append(forecast_element)
+
+        valid_time = str(fname_split[1])         # 20191113-12Z
+        valid_time_list.append(valid_time)
+
+        fcst_hour = str(fname_split[2][:-4])     # F24
+        fcst_hour_list.append(fcst_hour)
+
+        # here is where each image gets split into four panels, which requires the ordering in the
+        # images eictionary
         for key in image:
-            print(key)
-            crop_string = image[key]['crop']
-            ext = image[key]['extension']
-            print(crop_string,ext)
-            dst_file = myCrop(crop_string, src_file, dst_dir, ext)
+            data_source = key
+            crop_string = image[key]['crop']                
+
+            print(crop_string,data_source)
+            dst_file_name =  src_file[:-4] + '_' + data_source + '.PNG'
+            dst_path = os.path.join(src_dir,'modified',dst_file_name)
+            myCrop(src_path, dst_path, crop_string, False);
+            myResize(dst_path,crop_dim,False);
 
 
-os.chdir(dst_dir)
-for card in ('f24*','f36*','f48*','f60*'):
-    string = ' ' 
-    for mf in glob.glob(card):
-        #montage_files.append(os.path.join(dst_dir,mf))
-        geom = ' '
-        print(os.path.join(dst_dir,mf))
-#        if 'nbm' in mf:
-#            geom = ' -geometry 300x300+0+0 '
-#        else:
-#            geom = ' -geometry 400x400+300-6 '            
-        
-        string = string + ' ' + os.path.join(dst_dir,mf)
-    montage_file = 'montage_' + card[:-1] + '.png'
+all_fcst_hours = uniq(fcst_hour_list)
+all_fcst_hours.reverse()
+all_valid_time_strs = uniq(valid_time_list)
+all_data_srcs = [a for a in image.keys()]
+all_forecast_elements = uniq(forecast_element_list)
+valid_timestr = all_valid_time_strs[0]
 
-    cmd = 'montage' + string  + geom +  os.path.join(dst_dir,montage_file)
-    os.system(cmd)
-    print(cmd)
+
+# create forecast source montage each containing all forecast hours for the forecast element (and verification)
+# using a given forecast source
+for forecast_element in all_forecast_elements:
+    for data_src in all_data_srcs:
+        cmd = ' montage '
+        montage_fname = 'montage_' + forecast_element + '_' + data_src + '.png'
+        montage_fpath = os.path.join(dst_dir,montage_fname)
+        montage_title = '"' + forecast_element + ' ' + data_src + '"'
+        for fcst_hour in all_fcst_hours:
+            new_fname = forecast_element + '_' + valid_timestr + '_' + fcst_hour + '_' + data_src + '.PNG'
+            full_path = os.path.join(dst_dir,new_fname)
+            label_name = '"' + fcst_hour + '" '
+            fcst_string = '-label ' + label_name + ' -pointsize 22 ' + full_path + ' ' 
+            cmd = cmd + fcst_string
+
+        verif_filename = forecast_element + '_' + valid_timestr + '_F24_observed.PNG'
+        verif_full_path = os.path.join(dst_dir,verif_filename)
+        verif_label_name = '"Observed"'
+        verif_fcst_string = '-label ' + verif_label_name + ' -pointsize 22 ' + verif_full_path + ' '         
+        cmd = cmd + verif_fcst_string 
+        cmd = cmd + ' -title ' + montage_title + ' -pointsize 24 -geometry ' + crop_dim + '+10+6 ' + montage_fpath        
+        if data_src != 'observed':
+            print(cmd)
+            print('\n\n')
+            os.system(cmd)
+
+
+# create forecast hour montages each containing the different forecast source for that forecast hour
+for forecast_element in all_forecast_elements:
+    for fcst_hour in all_fcst_hours:
+        cmd = ' montage '
+        montage_fname = 'montage_' + forecast_element + '_' + fcst_hour + '.png'
+        montage_fpath = os.path.join(dst_dir,montage_fname)
+        montage_title = '"Valid ' + valid_timestr + ' ' + fcst_hour + '" ' 
+        for data_src in all_data_srcs:
+            new_fname = forecast_element + '_' + valid_timestr + '_' + fcst_hour + '_' + data_src + '.PNG'
+            full_path = os.path.join(dst_dir,new_fname)
+            label_name = '"' + forecast_element + ' ' + data_src + '" '
+            cmd = cmd + '-label ' + label_name + ' -pointsize 22 ' + full_path + ' ' 
+        cmd = cmd + ' -title ' + montage_title + ' -pointsize 24 -geometry ' + crop_dim + '+10+6 ' + montage_fpath        
+        if cmd is not None:
+            print(cmd)
+            print('\n\n')
+            os.system(cmd)
+
+            
 
 #montage_sorted = sorted(montage_files)
 #print(montage_sorted)
